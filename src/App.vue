f9859c5b53801288c91399754f559c47ae459c2e
SoulLution <faun1605@gmail.com>
2020-02-24 17:42:08 +0600

    check-6

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,7 @@
           </div>
         </div>
       </div>
-      <div class="body-developer" @mousedown="startTouch" @mouseup="endTouch">
+      <div class="body-developer">
         <div class="body-developer-title">О застройщике</div>
         <div class="body-developer-about">Осуществляем полный цикл работ от строительства до продажи квартир</div>
 
@@ -173,7 +173,7 @@
         this.touch = e.touches[0].clientX
       }
       document.body.ontouchend = e => {
-        let x = e.changedTouches[0].clientX, index = 0
+        let x = e.changedTouches[0].clientX, index
 
         if(this.touch - x > (document.body.clientWidth/2))
           index = 1
@@ -189,21 +189,6 @@
       setInterval(()=>this.switchPage(1),10000)
     },
     methods:{
-      startTouch(e){
-        this.touch = e.touches[0].clientX;
-      },
-      endTouch(e){
-        let x = e.changedTouches[0].clientX, index = 0
-
-        if(this.touch - x > (document.body.clientWidth/2))
-          index = 1
-        else if(x -this.touch > (document.body.clientWidth/2))
-          index = -1
-
-        if(index)
-          this.switchPage(index)
-        this.touch = 0;
-      },
       sendEmail(e){
         e.preventDefault();
         let data = {_replyto: 'help@nb-holding.kz'}
